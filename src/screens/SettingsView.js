3c554b0045f2f8f593d46d03436524802a41122c
Max Saienko <msaienko@ancestry.com>
2018-05-13 11:51:53 -0700

    initial admob settings

--- src/screens/SettingsView.js
+++ src/screens/SettingsView.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { AdMobBanner, AdMobInterstitial, AdMobRewarded } from 'expo';
 import { connect } from 'react-redux';
 import { View } from 'react-native';
 import {
     Container,
     H3,
     Text,
-    Button
+    Button,
+    Spinner
 } from 'native-base';
 
 import ScreenHeader from './components/screenHeader';
@@ -19,9 +21,91 @@ class SettingsView extends Component {
         drawerLabel: SCREEN_NAME
     });
 
+    constructor() {
+        super();
+        this.state = {
+            adIsLoading: false,
+            shouldUpdateAvatar: false
+        };
+    }
+
+    componentDidMount() {
+        // AdMobInterstitial.setTestDeviceID('EMULATOR');
+        // // ALWAYS USE TEST ID for Admob ads
+        // AdMobInterstitial.setAdUnitID('ca-app-pub-3940256099942544/1033173712');
+        // AdMobInterstitial.addEventListener('interstitialDidLoad', () => console.log('interstitialDidLoad'));
+        // AdMobInterstitial.addEventListener('interstitialDidFailToLoad', () => console.log('interstitialDidFailToLoad'));
+        // AdMobInterstitial.addEventListener('interstitialDidOpen', () => console.log('interstitialDidOpen'));
+        // AdMobInterstitial.addEventListener('interstitialDidClose', () => console.log('interstitialDidClose'));
+        // AdMobInterstitial.addEventListener('interstitialWillLeaveApplication', () => console.log('interstitialWillLeaveApplication'));
+
+        AdMobRewarded.setTestDeviceID('EMULATOR');
+        // ALWAYS USE TEST ID for Admob ads
+        AdMobRewarded.setAdUnitID('ca-app-pub-3940256099942544/1712485313');
+        AdMobRewarded.addEventListener('rewardedVideoDidRewardUser', () => {
+            console.log('rewardedVideoDidRewardUser');
+            this.setState({
+                shouldUpdateAvatar: true
+            });
+        });
+        AdMobRewarded.addEventListener('rewardedVideoDidLoad', () => console.log('rewardedVideoDidLoad'));
+        AdMobRewarded.addEventListener('rewardedVideoDidFailToLoad', () => {
+            console.log('rewardedVideoDidFailToLoad');
+            this.setState({
+                shouldUpdateAvatar: false,
+                adIsLoading: false
+            });
+        });
+        AdMobRewarded.addEventListener('rewardedVideoDidOpen', () => console.log('rewardedVideoDidOpen'));
+        AdMobRewarded.addEventListener('rewardedVideoDidClose', this.avatarVideoClosed.bind(this));
+        AdMobRewarded.addEventListener('rewardedVideoWillLeaveApplication', () => console.log('rewardedVideoWillLeaveApplication'));
+    }
+
+    componentWillUnmount() {
+        AdMobRewarded.removeAllListeners();
+    }
+
+    getButtonText(text) {
+        const { adIsLoading } = this.state;
+        if (adIsLoading === true) {
+            return (<Spinner color = 'gray' />);
+        }
+        return (<Text>{text}</Text>);
+    }
+
+    avatarVideoClosed() {
+        const { navigation, avatarType, updateUserAvatar } = this.props;
+        const { shouldUpdateAvatar } = this.state;
+        this.setState({
+            adIsLoading: false
+        });
+
+        if (shouldUpdateAvatar) {
+            this.setState({
+                shouldUpdateAvatar: false
+            });
+            updateUserAvatar(avatarType, navigation);
+        }
+    }
+
+    showRewardedToUpdateAvatar() {
+        // first - load ads and only then - show
+        this.setState({
+            adIsLoading: true,
+            shouldUpdateAvatar: false
+        });
+        AdMobRewarded.requestAdAsync().then(() => {
+            AdMobRewarded.showAd();
+        });
+    }
+
+    bannerError() {
+        console.log('An error');
+        return;
+    }
+
     render() {
         const { navigation, avatarType, updateUserAvatar } = this.props;
-        console.log('crash', avatarType);
         const { openDrawer } = navigation;
         return (
             <Container
@@ -46,11 +130,27 @@ class SettingsView extends Component {
                             Get different avatar style
                         </Text>
                     </Button>
+                    <Button
+                        disabled = {this.state.adIsLoading}
+                        onPress = {this.showRewardedToUpdateAvatar.bind(this)}
+                    >
+                        {this.getButtonText('Update')}
+                    </Button>
                 </Container>
             </Container>
         );
     }
 }
+
+// Bottom Banner
+// <AdMobBanner
+//     style = {styles.bottomBanner}
+//     bannerSize = "fullBanner"
+//     adUnitID = "ca-app-pub-3940256099942544/6300978111"
+//     // Test ID, Replace with your-admob-unit-id
+//     testDeviceID = "EMULATOR"
+//     didFailToReceiveAdWithError = {this.bannerError}
+// />
 
 const styles = {
     mainContainer: {
